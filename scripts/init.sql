964f505ad2cf0cef6a5f29e09aed4d8972dd9908
Moxxx1e <moxxxie07@gmail.com>
2021-01-08 00:42:53 +0300

    Add indexes

--- scripts/init.sql
+++ scripts/init.sql
@@ -5,6 +5,11 @@ CREATE TABLE IF NOT EXISTS rooms
     price       int         NOT NULL,
     created     timestamptz NOT NULL DEFAULT now()
 );
+CREATE INDEX cover_index ON rooms (id, description, price, created);
+CREATE INDEX price_order_by_asc_rooms ON rooms (price ASC);
+CREATE INDEX price_order_by_desc_rooms ON rooms (price DESC);
+CREATE INDEX created_order_by_asc_rooms ON rooms (created ASC);
+CREATE INDEX created_order_by_desc_rooms ON rooms (created DESC);
 
 CREATE TABLE IF NOT EXISTS bookings
 (
@@ -15,3 +20,6 @@ CREATE TABLE IF NOT EXISTS bookings
 
     FOREIGN KEY (room) REFERENCES rooms (id) ON DELETE CASCADE
 );
+CREATE INDEX cover_bookings ON bookings (id, date_start, date_end, room);
+CREATE INDEX room_bookings ON bookings (room);
+CREATE INDEX date_start_order_by_bookings ON bookings (date_start ASC);
